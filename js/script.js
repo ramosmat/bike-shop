e66f93f062dc94b3f47c1a692ab945944358d56f
Matheus <matheusramosbi.13@gmail.com>
2024-06-03 16:08:15 -0300

    add image-gallery change on click

--- js/script.js
+++ js/script.js
@@ -44,3 +44,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
